830c6c1ce71d08042b695932edf96d35f02a7f49
Christopher Murphy <christopher@pop-os.localdomain>
2019-02-18 08:44:56 +0100

    chore: install faker and faker types for random test data

--- src/data-structures/graph/Vertex.test.ts
+++ src/data-structures/graph/Vertex.test.ts
@@ -1,12 +1,18 @@
+import * as faker from 'faker'
 import Vertex from './Vertex'
 
 describe('The Vertex class', () => {
   test('create a new Vertex', () => {
-    expect(new Vertex(1, 'vertex one')).toBeInstanceOf(Vertex)
+    expect(new Vertex(1, faker.random.objectElement())).toBeInstanceOf(Vertex)
   })
 
   test('get the key from a vertex', () => {
-    const vertex = new Vertex(1, 'vertex one')
-    expect(vertex.key).toBe(1)
+    const vertex = new Vertex(faker.random.number(), faker.random.objectElement())
+    expect(typeof vertex.key).toBe('number')
+  })
+
+  test('get the data from a vertex', () => {
+    const vertex = new Vertex(1, faker.random.objectElement())
+    expect(vertex.data).toBeDefined()
   })
 })
